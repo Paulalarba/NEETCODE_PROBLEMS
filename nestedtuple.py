78117675d01b7e0069d133adea3434b121860a73
PAUL ALARBA <alarbapaulax@gmail.com>
2025-09-22 00:23:55 +0800

    nessted tuple and how to sort a list

---
+++ nestedtuple.py
@@ -0,0 +1,16 @@
+# 2. WRITE A FOR LOOP THAT LOOPS OVER DATA AND PRINTS THE SUM OF EACH
+#  NESTED TUPLE. THE OUTPUT SHOULD LOOK LIKE THIS
+# Row 1 sum: 3
+# Row 2 sum: 7
+nested = ((1,2), (3,4))
+print(nested)
+
+for i in range(len(nested)):
+    print(f"Row {i + 1} sum: {sum(nested[i])}")
+    
+nested_copy = nested[:]
+print(nested_copy)
+
+numbers = [5, 3, 8, 1]
+numbers.sort()              # sort in ascending order
+print(numbers)
